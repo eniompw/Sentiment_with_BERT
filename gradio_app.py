6b4f42a0201c80b7da29df03f19c6d9ff3d3a01f
eniompw <71254710+eniompw@users.noreply.github.com>
2022-09-30 13:51:44 +0100

    Create gradio_app.py

---
+++ gradio_app.py
@@ -0,0 +1,15 @@
+import gradio as gr
+import tensorflow as tf
+import tensorflow_text as text
+
+reloaded_model = tf.saved_model.load("./imdb_bert")
+
+def sentiment(text):
+    reloaded_results = tf.sigmoid(reloaded_model(tf.constant([text])))
+    if reloaded_results[0].numpy()[0] > 0.5:
+        return "Positive"
+    else:
+        return "Negative"
+
+iface = gr.Interface(fn=sentiment, inputs="text", outputs="text")
+iface.launch()
